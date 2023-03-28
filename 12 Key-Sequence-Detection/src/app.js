b61c7b5ea075b166a92d65540d75a94b4320d8c1
Subhadeep <subhadeeppaul35@gmail.com>
2023-03-28 13:53:56 +0530

    🔖 Added challenge-12

---
+++ 12 Key-Sequence-Detection/src/app.js
@@ -0,0 +1,18 @@
+// import ConfettiGenerator from "confetti-js";
+const pressed = [];
+const secretCode = "vishal";
+console.info(`Hint: Type ${secretCode} on your keyboard`);
+let confettiSettings = { target: "my-canvas", animate: true,size: 4, clock: 100 };
+
+window.addEventListener("keyup", e => {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  console.log(pressed);
+  if (pressed.join("").includes(secretCode)) {
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+  } else {
+    var confettii = new ConfettiGenerator(confettiSettings);
+    confettii.clear();
+  }
+});
