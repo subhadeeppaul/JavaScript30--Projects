2d502e294de077ce5c38bbbb10cd7f1b59e41525
Subhadeep <subhadeeppaul35@gmail.com>
2023-03-29 11:52:40 +0530

    Longitude and Lattitude added

--- 21 Geolocation/src/app.js
+++ 21 Geolocation/src/app.js
@@ -1,9 +1,13 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
+const latitude = document.querySelector('.latitude');
+const longitude = document.querySelector('.longitude');
 
 navigator.geolocation.watchPosition((data) => {
     console.log(data);
     speed.textContent = (data.coords.speed * 3.6).toFixed(2);
+    latitude.textContent = data.coords.latitude;
+    longitude.textContent = data.coords.longitude;
     arrow.style.transform = `rotate(${data.coords.heading}deg)`
 }, (err) => {
     console.log(err);
