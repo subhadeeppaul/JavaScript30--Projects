f31c927565fdc8bc65631f987722e07836927950
Subhadeep <subhadeeppaul35@gmail.com>
2023-03-29 11:03:50 +0530

    ✨ Project #17 Completed

---
+++ 17 Sort-without-Articles/src/app.js
@@ -0,0 +1,48 @@
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog"
+];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, "").trim();
+}
+
+const sortBandsByAsc = bands.sort((a, b) => (strip(a) < strip(b) ? -1 : 1));
+
+let bandsCopy = [...bands];
+const sortBandsByDesc = bandsCopy.sort((a, b) =>
+  strip(a) > strip(b) ? -1 : 1
+);
+
+const sortBtn = document.querySelector("#sort-btn");
+let sortByAsc = true;
+
+document.querySelector("#bands").innerHTML = sortBandsByAsc
+  .map(band => `<li>${band}</li>`)
+  .join("");
+
+sortBtn.addEventListener("click", () => {
+  sortByAsc = !sortByAsc;
+  if (sortByAsc) {
+    document.querySelector("#bands").innerHTML = sortBandsByAsc
+      .map(band => `<li>${band}</li>`)
+      .join("");
+    sortBtn.textContent = "Sort by Descending";
+  } else {
+    document.querySelector("#bands").innerHTML = sortBandsByDesc
+      .map(band => `<li>${band}</li>`)
+      .join("");
+    sortBtn.textContent = "Sort by Ascending";
+  }
+});
